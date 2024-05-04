014849bcb513acdebbda72580dfc232174fa3daa
Ashvin Antyah <a.antyah@gmail.com>
2024-05-04 13:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 const character = "!";
 const count = 10;
 const rows = [];
-let inverted=false;
+let inverted = false;
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);}
-
-for (let i =1; i <= count; i++) {
-    if (inverted) {
-            rows.unshift(padRow( i , count))
-
-    }else{
-        rows.push(padRow(i,count))
-
-    }
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
 
-while (rows.length < count) {
-    rows.push(padRow(rows.length + 1,count))
-    
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
-for (let i = count; i>0; i = i-- ){
-    rows.push(padRow(i,count))
-}
-
 
 let result = "";
 for (const row of rows) {
-    result =result + "\n" + row;
-
+  result = result + "\n" + row;
 }
 console.log(result);
